a8e973c2b0652b2771cc5a9b41e3f3db7f2fd7ec
Samar <cyparis.samura@yandex.ru>
2019-12-26 04:23:35 +0300

    Refactoring JS for portfolio

--- js/script.js
+++ js/script.js
@@ -1,17 +1,12 @@
-// Filtering works in the portfolio by type
-$(".button").click(function() {
-
-	// Click the button with the desired attribute filter is not equal "all"
-	if($(this).attr("filter") && $(this).attr("filter") !== "all"){
-
-		// If the filter attribute of the pressed button is not equal to the 
-		// filter attribute of the div, then the div smoothly disappears
-		$(".filter > div*[filter !='"+$(this).attr('filter')+"']").hide(300);
-
-		// If equal, then smoothly shown or remains visible
-		$(".filter > div*[filter ='"+$(this).attr('filter')+"']").show(300);
-		// Else, if is equal to "all", then the entire portfolio becomes visible
-	}else{
-		$(".filter > div").show(300);
-	}
+$(document).ready(function() {
+	$(".button[filter]").click(function() {
+		if ($(this).attr("filter") && $(this).attr("filter") !== "all") {
+			$(".filter > div*[filter !='" + $(this).attr('filter') + "']").hide();
+			$(".filter > div*[filter ='" + $(this).attr('filter') + "']").show();
+		} else {
+			$(".filter > div").show();
+		}
+		$(".button[filter]").removeClass('focused');
+		$(this).addClass('focused');
+	});
 });
